4c61f251e492b405ab372f4c5f2ad36e0d4970d3
Kelvin Jiang <kjiang@monarchpartners.com>
2017-12-05 15:29:01 -0500

    a

--- testNews/urls.py
+++ testNews/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.news, name='news'),
     url(r'^news/(?P<pk>\d+)/$', views.news_text, name='news_text')
-    url(r'^list/', views.news_list, name='news_list'),
+    url(r'^list/$', views.news_list, name='news_list'),
     # url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
     # url(r'^post/new/$', views.post_new, name='post_new'),
     # url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
