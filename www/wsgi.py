38cc9433db206d4b769c6626c528960d6922c465
investwise <hello@investwise.io>
2017-12-04 23:16:41 -0500

    local settings

--- www/wsgi.py
+++ www/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+path = 'D:/Dropbox/Investwise/www'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'www.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "www.settings")
-
-application = get_wsgi_application()
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
