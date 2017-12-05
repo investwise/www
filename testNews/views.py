1a9aca6251f682798199a1e60fc6eff425273b5a
Kelvin Jiang <kjiang@monarchpartners.com>
2017-12-05 13:39:25 -0500

    a

--- testNews/views.py
+++ testNews/views.py
@@ -10,11 +10,17 @@ def news(request):
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
+            news = form.save(commit=False)
+            news.url = request.user
             post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
-    return render(request, 'tutBlog/post_edit.html', {'form': form})
+    return render(request, 'testNews/post_edit.html', {'form': form})
+
+'''
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'tutBlog/post_detail.html', {'post': post})
+'''
