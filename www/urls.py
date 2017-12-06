481ac90a008dc12f6a8f4d8370f19afc2904159e
investwise <hello@investwise.io>
2017-12-05 19:51:17 -0500

    a

--- www/urls.py
+++ www/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
+import .views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
